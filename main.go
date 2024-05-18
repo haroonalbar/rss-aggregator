6d4f24ecd18263f279b76cd02407fe25ddf82876
Haroon Albar <haroonalbarx@gmail.com>
2024-05-19 01:52:33 +0530

    feat: getting the env from shell.

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
-  
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() { 
+  // This fails because PORT is not in the current shell env.
+  // It's not taken from the .env file
+  port := os.Getenv("PORT")
+  if port == ""{
+    log.Fatal("Port is not in the env")
+  }
+  fmt.Println("PORT: ",port)
 }
