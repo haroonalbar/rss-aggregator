3cb0b27b1d197fd6b59641811ab491c9af1a9931
Haroon Albar <haroonalbarx@gmail.com>
2024-05-19 01:56:39 +0530

    chore: notes

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 func main() { 
   // This fails because PORT is not in the current shell env.
   // It's not taken from the .env file
+  // we can add the env by using export command
+  // $ export PORT=8000
+  // the above command will add the env variable to the current shell 
+  // but to get it from the .env file we are going to use the godotenv package.
   port := os.Getenv("PORT")
   if port == ""{
     log.Fatal("Port is not in the env")
