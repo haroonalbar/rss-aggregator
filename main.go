37d7d9dd02a9ee6fa49f5748dcf343aef89d86d8
Haroon Albar <haroonalbarx@gmail.com>
2024-05-19 02:18:13 +0530

    feat: created a router and server using that router and port

--- main.go
+++ main.go
@@ -3,26 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
-func main() { 
-  // This fails because PORT is not in the current shell env.
-  // It's not taken from the .env file
-  // we can add the env by using export command
-  // $ export PORT=8000
-  // the above command will add the env variable to the current shell 
-  // but to get it from the .env file we are going to use the godotenv package.
+func main() {
+	// This fails because PORT is not in the current shell env.
+	// It's not taken from the .env file
+	// we can add the env by using export command
+	// $ export PORT=8000
+	// the above command will add the env variable to the current shell
+	// but to get it from the .env file we are going to use the godotenv package.
 
+	// this will load the env from the .env file
+	godotenv.Load()
 
-  // this will load the env from the .env file
-  godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not in the env")
+	}
+	fmt.Println("PORT: ", port)
 
-  port := os.Getenv("PORT")
-  if port == ""{
-    log.Fatal("Port is not in the env")
-  }
-  fmt.Println("PORT: ",port)
+	// adding a router
+	router := chi.NewRouter()
+
+	// creating a server
+	srv := http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
 }
