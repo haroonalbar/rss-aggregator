5498eea5c5bc78ef342c0f7c228683d5ab983b10
Haroon Albar <haroonalbarx@gmail.com>
2024-05-31 17:32:55 +0530

    feat: handle get feeds

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func main() {
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
 	// mount the v1Router to router so the whole path will become path/v1/ready
 	router.Mount("/v1", v1Router)
