a84351a118cc0d20bbc0040da80f47096c568413
Haroon Albar <haroonalbarx@gmail.com>
2024-06-06 14:27:53 +0530

    chore: fmt

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -13,6 +13,6 @@ LIMIT 1;
 
 -- name: MarkFeedAsFetched :one
 UPDATE feeds
-SET last_fetched_at = NOW(),updated_at = NOW()
+SET last_fetched_at = NOW(), updated_at = NOW()
 WHERE id=$1
 RETURNING *;
