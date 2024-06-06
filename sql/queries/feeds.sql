fa7da07a8cc4492e251d852a4ccb3b791681f8e6
Haroon Albar <haroonalbarx@gmail.com>
2024-06-06 14:51:38 +0530

    feat: mine update feed query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -3,6 +3,12 @@ INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
+-- name: UpdateFeed :one
+UPDATE feeds
+SET updated_at=NOw(), name=$3, url=$4
+WHERE id=$1 AND user_id=$2
+RETURNING *;
+
 -- name: GetFeeds :many
 SELECT * FROM feeds;
 
