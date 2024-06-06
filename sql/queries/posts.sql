5e024ce846dc007e32a594698458a68c2ad0970d
Haroon Albar <haroonalbarx@gmail.com>
2024-06-07 00:25:32 +0530

    feat: updated get feeds for user query

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -20,4 +20,8 @@ FROM
     posts
     JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
 WHERE
-    feed_follows.user_id = $1;
+    feed_follows.user_id = $1
+ORDER BY
+    posts.published_at DESC
+LIMIT
+    $2;
