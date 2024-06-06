868ae33f3e591cb398771b3a3162bf4bc91bc7c4
Haroon Albar <haroonalbarx@gmail.com>
2024-06-06 23:50:18 +0530

    feat: create posts from the scraped feed

--- scraper.go
+++ scraper.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/haroonalbar/rss-aggregater/internal/database"
 )
 
@@ -53,7 +56,37 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-    log.Println("Found post :", item.Title, " on feed :", feed.Name)
+
+		pubTime, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing date %v : %v", item.PubDate, err)
+			continue
+		}
+
+		description := sql.NullString{}
+
+		if item.Description != "" {
+			description = sql.NullString{String: item.Description, Valid: true}
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			Url:         item.Link,
+			PublishedAt: pubTime,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			// this is expected behaviour if the post already exists it shouldn't create a new one.
+			// so loging it is not needed
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Printf("Error creating post: %v", err)
+		}
 
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
